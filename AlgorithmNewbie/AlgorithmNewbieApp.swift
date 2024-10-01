5329bfeaaf8defdd30f56afbae8a6a1c6b15427b
Masayuki Ataka <masayuki.ataka@gmail.com>
2024-10-02 02:51:49 +0900

    新プロジェクト AlgorithmNewbie を作成した

---
+++ AlgorithmNewbie/AlgorithmNewbieApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmNewbieApp.swift
+//  AlgorithmNewbie
+//
+//  Created by 安宅正之 on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmNewbieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlgorithmNewbieTests/AlgorithmNewbieTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmNewbieTests.swift
+//  AlgorithmNewbieTests
+//
+//  Created by 安宅正之 on 2024/10/02.
+//
+
+import Testing
+@testable import AlgorithmNewbie
+
+struct AlgorithmNewbieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
