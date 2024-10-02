3df1a440a740843acb9ba5c9bcf113a88767faf6
Masayuki Ataka <masayuki.ataka@gmail.com>
2024-10-03 03:51:02 +0900

    線形探査法を使ったオープンアドレス法による HashTable を作成した
    削除状態を追加し、削除におけるバグを修正しました。

--- MyLibrary/Sources/Algorithm/HashTable.swift
+++ MyLibrary/Sources/Algorithm/HashTable.swift
@@ -150,3 +150,73 @@ public struct HashTable31<Key: MyHashable, Value> {
         (index + 1) % capacitySize
     }
 }
+
+/// 線形探査法を使ったオープンアドレス法による HashTable
+///
+/// 削除のケアをするために削除状態を追加した完全バージョン
+public struct HashTable32<Key: MyHashable, Value> {
+    private struct Bucket {
+        let key: Key
+        let value: Value
+    }
+    private enum State {
+        case empty
+        case exist(Bucket)
+        case deleted
+    }
+    private var storage: [State]
+    private var capacitySize: Int = 7
+
+    public init() {
+        storage = Array(repeating: .empty, count: capacitySize)
+    }
+
+    public func value(for key: Key) -> Value? {
+        let index = probe(for: key)
+        guard case .exist(let bucket) = storage[index] else { return nil }
+        return bucket.value
+    }
+
+    public mutating func set(_ value: Value?, for key: Key) {
+        if let value {
+            let index = probe(for: key)
+            storage[index] = .exist(Bucket(key: key, value: value))
+        } else {
+            remove(for: key)
+        }
+    }
+
+    public mutating func remove(for key: Key) {
+        let index = probe(for: key)
+        storage[index] = .deleted
+    }
+
+    private func probe(for key: Key) -> Int {
+        var index = hashFunction(for: key)
+
+        while_loop: while true {
+            switch storage[index] {
+            case .empty:
+                return index
+            case .exist(let bucket):
+                if bucket.key == key {
+                    break while_loop
+                }
+            case .deleted:
+                break
+            }
+
+            index = nextHashFunction(index: index)
+        }
+
+        return index
+    }
+
+    private func hashFunction(for key: Key) -> Int {
+        key.hashValue % capacitySize
+    }
+
+    private func nextHashFunction(index: Int) -> Int {
+        (index + 1) % capacitySize
+    }
+}
